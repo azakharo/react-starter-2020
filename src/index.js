1b72cc7d061a6c8fc9de9b815c94f54b1b2f62ac
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2020-12-03 15:20:36 +0300

    moved app container

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import AppContainer from 'src/components/AppContainer';
+import AppContainer from './AppContainer';
 
 const mountNode = document.getElementById('app');
 
